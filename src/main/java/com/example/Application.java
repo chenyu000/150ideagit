409f2d664bc0157d20b5bfc0f0bc0f0e59d3260d
chenyu <479523594@qq.com>
2019-05-31 11:27:02 +0800

    v5.0

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,11 +9,6 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        System.out.println();
-        System.out.println("hello world");
-        System.out.println();
-         System.out.println("I am zhangsan");
-        System.out.println();
     }
 
 }
